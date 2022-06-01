59fc5a28d9654af8cdd15f9bfc6cc69ea7fac4ed
Ronald Alfaro <ronald.alfaro91@gmail.com>
2022-06-01 17:24:43 -0600

    Configuration of Jetstream as base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    content: ["./resources/**/*.blade.php"],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [],
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
